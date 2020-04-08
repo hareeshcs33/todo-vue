a360551db81b2a5c256660e62d02f8d4f549f5ce
hareeshcs33 <hareeshcs33@gmail.com>
2020-04-08 20:24:55 +0530

    Added new Todo app repo

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="app-page">
+      <h1>Online shopping cart</h1>
+
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
